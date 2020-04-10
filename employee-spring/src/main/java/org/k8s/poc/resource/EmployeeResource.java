c1c5073998d7ac755a423c88a806dc028066b293
SERGIO RODRIGUEZ CALVO <SERGIO.RODRIGUEZ.CALVO@ing.com>
2020-04-10 15:14:52 +0200

    autowired

--- employee-spring/src/main/java/org/k8s/poc/resource/EmployeeResource.java
+++ employee-spring/src/main/java/org/k8s/poc/resource/EmployeeResource.java
@@ -1,5 +1,7 @@
 package org.k8s.poc.resource;
 
+import org.k8s.poc.service.EmployeeService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import reactor.core.publisher.Flux;
@@ -8,6 +10,9 @@ import java.util.stream.Stream;
 
 @RestController
 public class EmployeeResource {
+
+    @Autowired
+    private EmployeeService employeeService;
 
     @GetMapping("/spring/employee")
     public Flux<String> getEmployees() {
--- employee-spring/src/main/java/org/k8s/poc/service/EmployeeService.java
+++ employee-spring/src/main/java/org/k8s/poc/service/EmployeeService.java
@@ -1,7 +1,13 @@
 package org.k8s.poc.service;
 
+import org.k8s.poc.repository.EmployeeRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
 public class EmployeeService {
+
+    @Autowired
+    private EmployeeRepository employeeRepository;
+
 }
